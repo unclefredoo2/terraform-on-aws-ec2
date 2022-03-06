6759ac4fbfbbbb32ebaef63c23916635b051f8c9
unclefredoo2 <wilfred.okoro20@gmail.com>
2022-03-06 15:54:40 -0600

    update

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -1,14 +1,26 @@
 # Input Variables
-# AWS Region
 
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS will be created"
+  type = string
+  default = "us-east-1"
+  
 }
+
 
 # AWS EC2 Instance Type
+variable "Instance_type" {
+  description = "EC2 instance Type"
+  type = string
+  default = "t3.micro"
+  }
 
-}
 
 # AWS EC2 Instance Key Pair
-
+variable "instance_keypair" {
+  description = "AWS EC2 key pair that need to be associated with instance"
+  type = string
+  default = "mean"
 }
-
 
--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
@@ -1,1 +1,25 @@
 # Get latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "amz-linux2" {
+  executable_users = ["self"]
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
